00f86e16bb07212cc37adb4c0022aed6a61c55f0
mateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-01-08 16:46:41 -0300

    Modal de nova transação

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Transactions } from "./pages/Transactions";
+import { Theme } from "@radix-ui/themes";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <Theme>
+        <GlobalStyle />
 
-      <Transactions />
+        <Transactions />
+      </Theme>
     </ThemeProvider>
   );
 }
